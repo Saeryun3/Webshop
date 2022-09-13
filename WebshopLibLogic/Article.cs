88c3f837d377de1a259e68724f658cb3a61852f8
Saeryun3 <samed19982008@hotmail.com>
2022-09-13 15:27:14 +0200

    Add project files.

---
+++ WebshopLibLogic/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebshopLibLogic
+{
+    internal class Article
+    {
+        public int ArticleID { get; set; }
+        public string ArticleName { get; set; }       
+        public double ArticlePrice { get; set; }
+        public string ArticleDescription { get; set; }
+    }
+}
